bcc5dca3d2d692de8579832e72075aac581fc039
Jannik <125572791+Ja-Wu@users.noreply.github.com>
2023-08-01 22:53:36 +0000

    random color

--- page8.js
+++ page8.js
@@ -1,12 +1,6 @@
 function generateRandomAurora() {
     const elements = document.querySelectorAll('.aurora');
     elements.forEach((element) => {
-        // Random initial position
-        const randomX = Math.random() * (window.innerWidth - 500);
-        const randomY = Math.random() * (window.innerHeight - 500);
-        element.style.left = `${randomX}px`;
-        element.style.top = `${randomY}px`;
-
         // Random background gradient colors
         const randomColor1 = getRandomColor();
         const randomColor2 = getRandomColor();
@@ -15,9 +9,10 @@ function generateRandomAurora() {
 }
 
 function getRandomColor() {
-    // Generate a random RGB color
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
+    const alpha = Math.random().toFixed(2); // Alpha value between 0 and 1 with 2 decimal places
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
 }
+generateRandomAurora();
